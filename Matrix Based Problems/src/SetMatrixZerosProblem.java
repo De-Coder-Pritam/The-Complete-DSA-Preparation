f8e49d8ad33577f072ef9a4d162c3e4188c907b0
De-Coder-Pritam <pritamkumar2865@gmail.com>
2026-02-26 01:51:05 +0530

    Matrix Based Problems

--- Matrix Based Problems/src/SetMatrixZerosProblem.java
+++ Matrix Based Problems/src/SetMatrixZerosProblem.java
@@ -17,38 +17,91 @@ public class SetMatrixZerosProblem {
     Column 0 and column 3 → all elements become 0
      */
     public static void main(String[] args){
-        String columnTitle = "AA";
-        int n=columnTitle.length();
-        int total = 0;
-        for(int i=0;i<n;i++){
-            char ch = columnTitle.charAt(i);
-            int val = (ch-65+1) ;
 
-            total = total * 26 + val;
-
-
+        int[][] matrix = {
+                {0,1,2,0},
+                {3,4,5,2},
+                {1,3,1,5}
+        };
+        MatrixOperations.printMatrix(matrix);
+        setZeroesOptimal(matrix);
+        System.out.println("Matrix after Setting Zeros: ");
+        MatrixOperations.printMatrix(matrix);
+    }
+    public static void setZeroesOptimal(int[][] matrix){
+        boolean firstRowZero=false,firstColZero=false;
+        for(int i=0;i<matrix[0].length;i++){
+            if(matrix[0][i] == 0){
+                firstRowZero = true;
+                break;
+            }
         }
-        System.out.println(total);
-//        int[][] matrix = {
-//                {0,1,2,0},
-//                {3,4,5,2},
-//                {1,3,1,5}
-//        };
-//        MatrixOperations.printMatrix(matrix);
-//        setZeroesBrute(matrix);
-//        MatrixOperations.printMatrix(matrix);
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][0] == 0){
+                firstColZero = true;
+                break;
+            }
+        }
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
+                if(matrix[i][j] == 0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[i].length;j++){
+                if(matrix[0][j] == 0 || matrix[i][0]==0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(firstRowZero){
+            for(int i=0;i<matrix[0].length;i++){
+                matrix[0][i] = 0;
+            }
+        }
+        if(firstColZero){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+    public static void setZeroesBetter(int[][] matrix){
+        int[] rows = new int[matrix.length];
+        int[] cols = new int[matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    rows[i]=1;
+                    cols[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(rows[i]==1 || cols[j] == 1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
     }
 
     public static void setZeroesBrute(int[][] matrix) {
         // Your code goes here
+        int count=0;
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[i].length;j++){
+
                 if(matrix[i][j]==0){
                     markRows(matrix,i);
                     markColumns(matrix,j);
+                    count++;
                 }
             }
         }
+
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[i].length;j++){
                 if(matrix[i][j]==-1){
@@ -58,13 +111,15 @@ public class SetMatrixZerosProblem {
         }
     }
     public static void markRows(int[][] mat, int i){
-        for(int j=0;j<mat[0].length;j++){
-            mat[j][i]=-1;
+        for(int j=0;j<mat[i].length;j++){
+            if(mat[i][j]!=0)
+            mat[i][j]=-1;
         }
     }
     public static void markColumns(int[][] mat, int i){
         for(int j=0;j<mat.length;j++){
-            mat[i][j]=-1;
+            if(mat[j][i] != 0)
+            mat[j][i]=-1;
         }
     }
 }
