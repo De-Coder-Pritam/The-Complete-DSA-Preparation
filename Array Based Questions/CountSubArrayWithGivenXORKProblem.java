70372e24b423b17ac11efb8109f0c76273a4410d
De-Coder-Pritam <pritamkumar2865@gmail.com>
2026-03-15 00:33:06 +0530

    XOR Based Problem

--- Array Based Questions/CountSubArrayWithGivenXORKProblem.java
+++ Array Based Questions/CountSubArrayWithGivenXORKProblem.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class CountSubArrayWithGivenXORKProblem {
 //    Given an array of integers nums and an integer k,
 //    return the total number of subarrays whose XOR equals to k.
@@ -11,6 +13,36 @@ public class CountSubArrayWithGivenXORKProblem {
 //    Output : 2
 //    Explanation : The subarrays having XOR of their elements as 5 are [5] and [5, 6, 7, 8, 9]
     public static void main(String[] args){
-
+        int[] nums = {5, 6, 7, 8, 9};
+        int k = 5;
+        System.out.println(subarraysWithXorKOptimal(nums,k));
+    }
+    public static int subarraysWithXorKOptimal(int[] nums, int k) {
+        HashMap<Integer,Integer> prefix = new HashMap<>();
+        int xor = 0;
+        int count = 0;
+        prefix.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            xor ^= nums[i];
+            int target = xor ^ k;
+            if(prefix.containsKey(target)){
+                count += prefix.get(target);
+            }
+            prefix.put(xor,prefix.getOrDefault(xor,0)+1);
+        }
+        return count;
+    }
+    public static int subarraysWithXorKBrute(int[] nums, int k) {
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            int xor = 0;
+            for(int j=i;j<nums.length;j++){
+                xor ^= nums[j];
+                if(xor == k){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
